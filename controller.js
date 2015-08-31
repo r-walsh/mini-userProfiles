679d22721d9b316e4967fdd2adda2a031aaa8ebf
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-08-31 15:57:36 -0600

    updated with  to adjust data before passing to controller

--- controller.js
+++ controller.js
@@ -5,7 +5,7 @@ app.controller('MainController', function($scope, mainService) {
 	$scope.getUsers = function() {
 		mainService.getUsers()
 			.then(function(response) {
-				$scope.users = response.data.data;
+				$scope.users = response;
 			});
 	}
 
--- service.js
+++ service.js
@@ -1,8 +1,16 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-  	return $http.get('http://reqr.es/api/users?page=1');
+  	var deferred = $q.defer();
+  	$http.get('http://reqr.es/api/users?page=1').then(function(response) {
+  		var parsedResponse = response.data.data
+  		for (var i = 0; i < parsedResponse.length; i++) {
+  			parsedResponse[i].first_name = 'Ralf';
+  		}
+  		deferred.resolve(parsedResponse);
+  	});
+  	return deferred.promise;
   }
 });
