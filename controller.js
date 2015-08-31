203d206a218c0880c2883da8e4fc7ddc657fd817
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-08-31 08:37:15 -0600

    Updated for second mini project

--- controller.js
+++ controller.js
@@ -3,8 +3,10 @@ var app = angular.module('userProfiles');
 app.controller('MainController', function($scope, mainService) {
 
 	$scope.getUsers = function() {
-		$scope.users = mainService.getUsers();
-		console.log($scope.users);
+		mainService.getUsers()
+			.then(function(response) {
+				$scope.users = response.data.data;
+			});
 	}
 
 	$scope.getUsers();
